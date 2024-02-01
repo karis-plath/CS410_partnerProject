5959b9d8c88d2ac0ebd529f3de2452f273b7888e
pwnedmk <129480023+pwnedmk@users.noreply.github.com>
2024-02-01 04:36:17 -0600

    Update update_table.php

--- update_table.php
+++ update_table.php
@@ -1,28 +1,33 @@
 <?php
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
 $servername = "localhost";
 $username = "Myles"; 
 $password = "password";
 $dbname = "410flashcards";
 $conn = new mysqli($servername, $username  , $password, $dbname);
- 
 if ($conn->connect_errno > 0) {
     die('Unable to connect to database [' . $db->connect_error . ']');
 }
 else{
     $termArray = json_decode($_POST["termArray"]);
     $defArray = json_decode($_POST["defArray"]); 
-    $deckName = $_POST["deckName"]
+    $deckName = $_POST["deckName"];
     $dropTable = "DROP TABLE IF EXISTS " . $_SESSION["username"] . "_" . $deckName;
     $conn->query($dropTable);
-    $createTable = "CREATE TABLE " . $_SESSION["user"] . "_" . $deckName . " 
-    (ID int not null auto_increment, term varchar(50), definition varchar(255), primary key (ID))";
+    $createTable = "CREATE TABLE " . $_SESSION["username"] . "_" . $deckName . " (ID int not null auto_increment, term varchar(50), definition varchar(255), primary key (ID))";
+    $result = $conn->query($createTable);
+    if (!$result) {
+        die("Query failed: " . $conn->error);
+    }
+    print_r($result);
     for ($i = 0; $i < count($termArray); $i++) {
         $term = $termArray[$i];
         $definition = $defArray[$i];
         $insertQuery = "INSERT INTO " . $_SESSION["username"] . "_" . $deckName . " (term, definition) VALUES ('$term', '$definition')";
         $conn->query($insertQuery);
     }
-
+    echo '<script>alert("Inserted Values");</script>';
     
 }
 ?>
