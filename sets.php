0ab259265b230e535b89993d815f85d3cdccacde
pwnedmk <129480023+pwnedmk@users.noreply.github.com>
2024-02-01 05:47:03 -0600

    update

--- sets.php
+++ sets.php
@@ -50,7 +50,7 @@ $oldTerms = 0;
         die('Unable to connect to database [' . $db->connect_error . ']');
     }
     else{
-        $selectTerms = "SELECT * FROM `" . $_SESSION["Username"] . "_" . $deck . "`";
+        $selectTerms = "SELECT * FROM `" . $_SESSION["username"] . "_" . $deck . "`";
         $result = $conn->query($selectTerms);
         if ($result){
             while ($row = $result->fetch_assoc()){
@@ -137,6 +137,7 @@ $oldTerms = 0;
             document.getElementById("defArrayIn").value = JSON.stringify(defValues);
             var deckName = $("#deckName").val().trim();
             document.getElementById("deckNameInput").value = deckName;
+            
         });
     });
     </script>
--- update_table.php
+++ update_table.php
@@ -7,15 +7,18 @@ $password = "password";
 $dbname = "410flashcards";
 $conn = new mysqli($servername, $username  , $password, $dbname);
 if ($conn->connect_errno > 0) {
+    echo("<p>hello</p>");
     die('Unable to connect to database [' . $db->connect_error . ']');
 }
 else{
     $termArray = json_decode($_POST["termArray"]);
     $defArray = json_decode($_POST["defArray"]); 
     $deckName = $_POST["deckName"];
-    $dropTable = "DROP TABLE IF EXISTS " . $_SESSION["username"] . "_" . $deckName;
+    echo("<p>hello</p>");
+    echo("<script>console.log(" . $deckName . ")</script>");
+    $dropTable = "DROP TABLE IF EXISTS myles_" . $deckName;
     $conn->query($dropTable);
-    $createTable = "CREATE TABLE " . $_SESSION["username"] . "_" . $deckName . " (ID int not null auto_increment, term varchar(50), definition varchar(255), primary key (ID))";
+    $createTable = "CREATE TABLE myles_" . $deckName . " (ID int not null auto_increment, term varchar(50), definition varchar(255), primary key (ID))";
     $result = $conn->query($createTable);
     if (!$result) {
         die("Query failed: " . $conn->error);
@@ -24,7 +27,7 @@ else{
     for ($i = 0; $i < count($termArray); $i++) {
         $term = $termArray[$i];
         $definition = $defArray[$i];
-        $insertQuery = "INSERT INTO " . $_SESSION["username"] . "_" . $deckName . " (term, definition) VALUES ('$term', '$definition')";
+        $insertQuery = "INSERT INTO myles_" . $deckName . " (term, definition) VALUES ('$term', '$definition')";
         $conn->query($insertQuery);
     }
     echo '<script>alert("Inserted Values");</script>';
